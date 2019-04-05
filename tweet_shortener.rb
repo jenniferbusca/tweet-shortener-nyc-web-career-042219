2ec16ebcef6f321a50f52a1f6d8a01e4b2e8af20
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-05 17:16:06 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,9 +41,8 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    if tweet.length <= 140
+def selective_tweet_shortener(tweet)
+    if tweet.values.length <= 140
       puts tweet
     else
       puts word_substituter(tweet)
